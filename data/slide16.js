2e4a4c52fc27e2452f2d329014a6bee56764811b
kanisornNopparat <129506095+kanisornNopparat@users.noreply.github.com>
2023-03-31 17:40:19 +0700

    Initial commit

---
+++ data/slide16.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{E377233A-4641-4ADA-9BBC-5EC189295C5D}'];loadHandler&&loadHandler(15, '<div id="spr0_533287d"><div id="spr1_533287d" class="kern slide"><img id="img0_533287d" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_533287d" class="kern slide"></div></div>', '{"s":[]}');})();
